cfc07ed61f9e1b48ae3f2e6fcf8b7408575df99e
Karina Zubko <karinazubko2017@gmail.com>
2023-04-20 15:48:10 +0200

    performed laboratory

--- lab_4_zubko.java
+++ lab_4_zubko.java
@@ -1,17 +1,159 @@
 import java.util.Random;
 
 public class lab_4_zubko{
-    
+    public static void F(int n) {
+        if (n <= 0) return;
+        System.out.println(n);
+        F(n - 2);
+        F(n - 3);
+        System.out.println(n);
+    }
+    public static void F1(int n) {
+        if (n <= 0) {
+            return;
+        }
+        F1(n - 3);
+        System.out.println(n);
+        F1(n - 2);
+        System.out.println(n);
+    }
+    public static void F2(int n) {
+        if (n <= 0) {
+            return;
+        }
+        F2(n - 3);
+        F2(n - 2);
+        System.out.println(n);
+        System.out.println(n);
+    }
+    public static int F3(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+        if (b % 2 == 0) {
+            return F3(a + a, b / 2);
+        }
+        return F3(a + a, b / 2) + a;
+    }
+    public static int F4(int a, int b) {
+        if (b == 0) return 0;
+        if (b % 2 == 0) return F4(a+a, b/2);
+        return F4(a+a, b/2) + a;
+    }
+    public static int F5(int a, int b) {
+        if (b == 0) return 0;
+        else return a + F5(a, b-1);
+    }
+    public static int F6(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+        return 2*F6(n-2) + F6(n-3);
+    }
+    public static void IntToBinary(int number) {
+        if (number > 1) {
+            IntToBinary(number / 2);
+        }
+        System.out.print(number % 2);
+    }
+    public static void piramida(int level) {
+        if (level == 0) { 
+            return; 
+        }
+        for (int i = 0; i < level; i++) { 
+            System.out.print("*"); 
+        }
+        System.out.println(); 
+        piramida(level - 1); 
+    }
+    public static void piramida1(int level) {
+        if (level == 1) { 
+            System.out.println("*");
+        } else { 
+            piramida(level - 1); 
+            for (int i = 0; i < level; i++) { 
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+    }
+        public static double H1(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return 1.0 / (n * n) + H1(n - 1);
+    }
+    public static int H2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + H2(n - 1);
+    }
+    public static int H3(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return 2 * n + H3(n - 1);
+    }
+    public static int H4(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * (n + 1) * (2 * n + 1) / 6 + H4(n - 1);
+    }
     public static void main(String[] args){
         //task 1
-        
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("F(" + i + "):");
+            F(i);
+            System.out.println();
+        }
         //task 2 
-        
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Вызов F1(" + i + "):");
+            F1(i);
+            System.out.println("--------------");
+        }
         //task 3
-        
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Вызов F2(" + i + "):");
+            F2(i);
+            System.out.println("--------------");
+        }
         //task 4
-        
+        int a = 4;
+        int b = 7;
+        int result = F3(a, b);
+        System.out.println(a + " * " + b + " = " + result);
         //task 5
-       
+        int a1 = 5;
+        int b1 = 3;
+        int result1 = F4(a1, b1);
+        System.out.println("Res: " + result1);
+        //task 6
+        int result2 = F5(1, 6);
+        System.out.println(result2); 
+        //task 7
+        int result3 = F6(6);
+        System.out.println("F6(6) = " + result3);
+        //task 8
+        int number = 366;
+        IntToBinary(number);
+        //task 9
+        piramida(75);
+        //task 10
+        piramida1(75);
+        //task 11
+        int n = 5;
+        System.out.println("H1(" + n + ") = " + H1(n));
+        System.out.println("H2(" + n + ") = " + H2(n));
+        System.out.println("H3(" + n + ") = " + H3(n));
+        System.out.println("H4(" + n + ") = " + H4(n));
+        //task 12
+        //task 13
+        //task 14
+        //task 15
+        //task 16
+        //task 17
     }
 }
