3f7bbe1c880e297eab20965c0ecf504a191f9534
Karina Zubko <karinazubko2017@gmail.com>
2023-05-06 21:41:25 +0200

    Add insertion sort method to lab_5_zubko

--- lab_5_zubko.java
+++ lab_5_zubko.java
@@ -27,6 +27,7 @@ public class lab_5_zubko{
             arr[j + 1] = key;
         }
     }
+    
     public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
